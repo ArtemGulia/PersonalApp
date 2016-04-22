e596b270771fbaf9397b447a5983252a10f999c3
Artem Gulia <artem.gulia@masterdata.ru>
2016-04-22 16:34:29 +0300

    working on widget

--- app/src/main/java/com/g_art/personalapp/activity/PersonalActivity.java
+++ app/src/main/java/com/g_art/personalapp/activity/PersonalActivity.java
@@ -1,4 +1,4 @@
-package com.g_art.personalapp;
+package com.g_art.personalapp.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.g_art.personalapp.R;
 
 public class PersonalActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
---
+++ app/src/main/java/com/g_art/personalapp/widget/WidgetProvider.java
@@ -0,0 +1,40 @@
+package com.g_art.personalapp.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.util.Log;
+
+import java.util.Arrays;
+
+/**
+ * Created by fftem on 22-Apr-16.
+ */
+public class WidgetProvider extends AppWidgetProvider {
+    final String LOG_TAG = "myLogs";
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+        Log.d(LOG_TAG, "onEnabled");
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+                         int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+        Log.d(LOG_TAG, "onUpdate " + Arrays.toString(appWidgetIds));
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        super.onDeleted(context, appWidgetIds);
+        Log.d(LOG_TAG, "onDeleted " + Arrays.toString(appWidgetIds));
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+        Log.d(LOG_TAG, "onDisabled");
+    }
+}
